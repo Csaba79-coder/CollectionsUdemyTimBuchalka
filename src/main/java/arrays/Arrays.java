58b8214e2ebe700714627fd30f08940e92aeb428
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-02-24 17:17:23 +0100

    Descending and Ascending array

---
+++ src/main/java/arrays/Arrays.java
@@ -0,0 +1,47 @@
+package arrays;
+
+public class Arrays {
+
+    public void printArray(int[] array) {
+        for (int num : array) {
+            System.out.println(num);
+        }
+    }
+
+    public int[] sortedDescendingArrays(int[] array) {
+        int[] sortedArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+
+        boolean flag = true;
+        int temp;
+        
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+
+    public int[] sortedAscendingArrays(int[] array) {
+        int temp;
+        for (int i = 0; i <array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[i] > array[j]) {      //swap elements if not in order
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
